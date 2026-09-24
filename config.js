bbd7be0c4725224c07cfc6032a2f59ddef804bc5
zalabakatansi-hue <zalabakatansi@gmail.com>
2026-09-25 00:19:57 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// Supabase public browser configuration. The anon key is designed for browser use.
+// Do NOT put a Supabase service_role key here.
+window.SUPABASE_CONFIG = {
+  url: "PASTE_YOUR_SUPABASE_PROJECT_URL_HERE",
+  anonKey: "PASTE_YOUR_SUPABASE_ANON_KEY_HERE"
+};
